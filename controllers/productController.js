ea8ddb4edf80629b55e229d308cb9af07a48afeb
alexandrostsoulakidis <al.tsoulakidis@gmail.com>
2025-02-05 12:49:59 +0100

    dummy testing

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,7 +6,49 @@ const Product = require('../models/Product');
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
+  let products = await Product.find({});
+
+  // If no products are found, return dummy products for testing
+  if (products.length === 0) {
+    const dummyProducts = [
+      {
+        _id: 'dummy1',
+        name: 'Hoodlum T-Shirt',
+        price: 19.99,
+        image: '/images/tshirt.png', // Ensure you have an image at this path or use a URL
+        description: 'A stylish T-shirt with the Hoodlum Mentality logo.',
+        brand: 'Hoodlum',
+        category: 'Apparel',
+        countInStock: 15,
+        numReviews: 0,
+      },
+      {
+        _id: 'dummy2',
+        name: 'Hoodlum Hoodie',
+        price: 39.99,
+        image: '/images/hoodie.png', // Ensure this image exists or use a URL
+        description: 'A cozy hoodie with a bold design.',
+        brand: 'Hoodlum',
+        category: 'Apparel',
+        countInStock: 10,
+        numReviews: 0,
+      },
+      {
+        _id: 'dummy3',
+        name: 'Hoodlum Cap',
+        price: 14.99,
+        image: '/images/cap.png', // Ensure this image exists or use a URL
+        description: 'A cool cap to complete your look.',
+        brand: 'Hoodlum',
+        category: 'Accessories',
+        countInStock: 20,
+        numReviews: 0,
+      },
+    ];
+    return res.json(dummyProducts);
+  }
+
+  // Otherwise, return actual products from the database
   res.json(products);
 });
 
